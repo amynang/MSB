90815cc74de1f371ba998dd7447635bfdf716e81
Angelos Amyntas <61004068+amynang@users.noreply.github.com>
2024-07-16 09:23:39 +0200

    done

--- Chapter2/ch2_0_particleworld.jl
+++ Chapter2/ch2_0_particleworld.jl
@@ -4,7 +4,7 @@ using Random, LinearAlgebra
 @agent struct Particle(ContinuousAgent{2,Float64})
     speed::Float64
     whimsy::Float64
-    collisions::Int64
+    #collisions::Int64
 end
 
 function agent_step!(particle, model)
@@ -16,10 +16,11 @@ function agent_step!(particle, model)
     end
     # if any particles nearby, make a u-turn
     if count_neighbors > 0
-        randomwalk!(particle, model, 0.1, polar = Uniform(0.99*π,1.01*π))
+        randomwalk!(particle, model, 0.2, polar = Uniform(0.99*π,1.01*π))
     end
     new_collisions = count_neighbors
-    particle.collisions += new_collisions
+    #particle.collisions += new_collisions this would be the way to track collisions at agent level
+    model.total_collisions += new_collisions # instead we can track model-level collisions (Agents developers advise against it https://tinyurl.com/f384tpub)
     # normal movement
     randomwalk!(particle, model, particle.speed, polar = Uniform(-particle.whimsy*π/180,particle.whimsy*π/180))
 
@@ -29,14 +30,16 @@ function initialize_model(;
     n_particles = 50,
     speed = 1,
     whimsy = 45,
-    collisions = 0,
+    #collisions = 0,
+    total_collisions = 0,
     extent = (100, 100),
-    seed = 42,
+    seed = 123,
 )
     space2d = ContinuousSpace(extent; spacing = 1/1.5, periodic = true)
+    properties = Dict(:total_collisions => 0)
     rng = Random.MersenneTwister(seed)
 
-    model = StandardABM(Particle, space2d; rng, agent_step!, 
+    model = StandardABM(Particle, space2d; rng, agent_step!, properties,
                         container = Vector, # agents are not removed, so we use this
                         scheduler = Schedulers.Randomly())
     for _ in 1:n_particles
@@ -47,7 +50,7 @@ function initialize_model(;
             vel,
             speed,
             whimsy,
-            collisions,
+            #collisions,
         )
     end
     return model
@@ -58,7 +61,7 @@ model = initialize_model()
 
 using CairoMakie
 
-# this part does not work (i.e abmplot throws error, see issue #1045)
+# creating a custom marker shape
 const particle_polygon = Makie.Polygon(Point2f[(-1, -1), (2, 0), (-1, 1)])
 function particle_marker(b::Particle)
     φ = atan(b.vel[2], b.vel[1]) #+ π/2 + π
@@ -66,21 +69,23 @@ function particle_marker(b::Particle)
 end
 
 abmvideo(
-    "flocking.mp4", model;
-    #agent_marker = particle_marker, # here is the bit that results in error
+    "particle_world.mp4", model;
+    agent_marker = particle_marker,
+    agent_size = 5,
     framerate = 10, frames = 500,
-    title = "Flocking"
+    title = "Particle World"
 )
 
-
-adata = [(:collisions, sum)]
-adf, mdf = run!(model, 100; adata)
-
+#adata = [(:collisions, sum)]
+mdata = [:total_collisions]
+#adf, mdf = run!(model, 100; adata)
+mdf = run!(model, 100; mdata)
 
 using GLMakie # using a different plotting backend that enables interactive plots
 
 fig, abmobs = abmexploration(
     model; 
+    agent_marker = particle_marker,
     adata
 )
 fig
